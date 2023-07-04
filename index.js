0583a5239dd154c41a718766ffb52f92413c7410
mohamedyasser27 <my2722000@gmail.com>
2023-07-04 12:33:59 +0300

    Refactor the whole code to make it extendable

--- index.js
+++ index.js
@@ -1,53 +1,13 @@
-let brushColor = "";
-let brushColors = ["Black", "Random", "Eraser", "color"];
-
-let brushTypes = ["mouseover", "click"];
-let brushType = brushTypes[0];
+let brushColor = "#fff";
+let gridCellsCount = 16;
 const gridContainer = document.querySelector(".Grid-Container");
-let gridCells = Array.from(document.querySelectorAll(".Grid-Container>div"));
-gridContainer.addEventListener("mouseover", (e) => {
-  let boxXCoord = e.currentTarget.getBoundingClientRect().x;
-  let boxYCoord = e.currentTarget.getBoundingClientRect().y;
-  let cellXCoord = e.target.getBoundingClientRect().x;
-  let cellYCoord = e.target.getBoundingClientRect().y;
+let gridCells = [];
+const rainbowBrushBtn = document.querySelector(".rainbowBrushBtn");
+const eraserBtn = document.querySelector(".eraserBtn");
+const blackBrushBtn = document.querySelector(".blackBrushBtn");
+const fillBoardBtn = document.querySelector(".fillBoardBtn");
 
-  let boxWidth = e.currentTarget.getBoundingClientRect().width;
-  let boxHeight = e.currentTarget.getBoundingClientRect().width;
-});
-
-const colorPicker = document.querySelector("#colorPicker");
-colorPicker.addEventListener("input", (e) => {
-  const colorValue = e.target.value;
-  brushColor = colorValue;
-  brushType = brushTypes[0];
-  const gridCells = Array.from(
-    document.querySelectorAll(".Grid-Container>div")
-  );
-
-  gridCells.forEach((cell) => {
-    cell.addEventListener("mouseover", () => {
-      cell.brushColor = brushColor;
-      cell.style.backgroundColor = brushColor;
-    });
-  });
-});
-colorPicker.addEventListener("click", (e) => {
-  const colorValue = e.target.value;
-  brushColor = colorValue;
-  brushType = brushTypes[0];
-  const gridCells = Array.from(
-    document.querySelectorAll(".Grid-Container>div")
-  );
-
-  gridCells.forEach((cell) => {
-    cell.addEventListener("mouseover", () => {
-      cell.brushColor = brushColor;
-      cell.style.backgroundColor = brushColor;
-    });
-  });
-});
-
-function MakeGrid(numofGridItems) {
+async function MakeGrid(numofGridItems) {
   gridContainer.innerHTML = ""; //clear the grid for resizing
   gridContainer.style[
     "grid-template-columns"
@@ -59,125 +19,40 @@ function MakeGrid(numofGridItems) {
   let gridItemWidth = gridWidth / numofGridItems;
   let gridItemHeight = gridHeight / numofGridItems;
 
-  numofGridItems *= numofGridItems;
-  for (let counter = 1; counter <= numofGridItems; counter++) {
+  for (let counter = 1; counter <= numofGridItems * numofGridItems; counter++) {
     let gridItem = document.createElement("div");
     gridItem.style.height = `${gridItemHeight}px`;
     gridItem.style.width = `${gridItemWidth}px`;
+    gridItem.setAttribute("randomColor", false);
+
     gridContainer.appendChild(gridItem);
   }
-  handlecells();
+  gridCells = Array.from(document.querySelectorAll(".Grid-Container>div"));
 }
 
-function ColorGenerator(brush) {
-  let color = ``;
-  if (brush == brushColors[0]) {
-    color = `rgb(0, 0, 0)`;
-  } else if (brush == brushColors[1]) {
-    let red = Math.floor(Math.random() * 256),
-      green = Math.floor(Math.random() * 256),
-      blue = Math.floor(Math.random() * 256);
-    color = `rgb(${red}, ${green}, ${blue})`;
-  } else {
-    color = `rgb(255, 255, 255)`;
-  }
-  return color;
-}
+function setupGridCellHoverListener() {
+  gridContainer.addEventListener("mouseover", ({ currentTarget, target }) => {
+    let boxXCoord = currentTarget.getBoundingClientRect().x;
+    let boxYCoord = currentTarget.getBoundingClientRect().y;
+    let cellXCoord = target.getBoundingClientRect().x;
+    let cellYCoord = target.getBoundingClientRect().y;
+    let boxWidth = currentTarget.getBoundingClientRect().width;
+    let positionX = (cellXCoord - boxXCoord) / (boxWidth / gridCellsCount);
+    let positionY = Math.trunc(
+      (cellYCoord - boxYCoord) / (boxWidth / gridCellsCount)
+    );
+    let cellIndex = positionY * gridCellsCount + positionX;
 
-function changeGridSize() {
-  const changeSizeBtn = document.querySelectorAll(".Buttons>button")[0];
-  changeSizeBtn.addEventListener("click", () => {
-    console.log(brushColor);
-    let numoFGridItems = Array.from(
-      document.querySelectorAll(".Grid-Container>div")
-    ).length;
-    numoFGridItems === 256
-      ? MakeGrid(Math.sqrt(numoFGridItems) * 2)
-      : MakeGrid(Math.sqrt(numoFGridItems) / 2);
-  });
-}
-
-function changeBrushType() {
-  if (brushType == brushTypes[0]) {
-    gridCells.forEach((cell) => {
-      cell.onmouseover = null;
-    });
-    brushType = brushTypes[1];
-  } else if (brushType == brushTypes[1]) {
-    gridCells.forEach((cell) => {
-      cell.onmouseover = CellEventHandler.bind(cell);
-    });
-    brushType = brushTypes[0];
-  }
-}
-function AddButtonEvents() {
-  let buttons = Array.from(document.querySelectorAll(".Buttons>button"));
-  buttons.forEach((button, index) => {
-    if (index == buttons.length - 3) {
-      button.addEventListener("click", () => {
-        let numoFGridItems = Array.from(
-          document.querySelectorAll(".Grid-Container>div")
-        ).length;
-        MakeGrid(Math.sqrt(numoFGridItems));
-      });
-      console.log(brushColor);
-    } else if (index == buttons.length - 1) {
-      button.addEventListener("click", () => {
-
-        let gridCells = Array.from(
-          document.querySelectorAll(".Grid-Container>div")
-        );
-
-        if (brushType == brushTypes[0]) {
-          gridCells.forEach((cell) => {
-            cell.onmouseover = null;
-          });
-          brushType = brushTypes[1];
-        } else if (brushType == brushTypes[1]) {
-          gridCells.forEach((cell) => {
-            cell.onmouseover = CellEventHandler.bind(cell);
-          });
-          brushType = brushTypes[0];
-        }
-      });
+    if (brushColor == "random") {
+      if (gridCells[cellIndex].getAttribute("randomColor") != "true") {
+        gridCells[cellIndex].setAttribute("randomColor", true);
+        gridCells[cellIndex].style.backgroundColor = ColorGenerator();
+      }
     } else {
-      button.addEventListener("click", () => {
-        brushColor = brushColors[index - 1];
-      });
+      gridCells[cellIndex].setAttribute("randomColor", false);
+      gridCells[cellIndex].style.backgroundColor = brushColor;
     }
   });
-}
-
-function handlecells() {
-  let gridCells = Array.from(document.querySelectorAll(".Grid-Container>div"));
-  gridCells.forEach((cell) => {
-    cell.onclick = CellEventHandler.bind(cell, cell);
-    cell.onmouseover = CellEventHandler.bind(cell, cell);
-  });
-}
-
-function CellEventHandler(cell) {
-  if (brushColor == brushColors[0]) {
-    if (cell.brushColor == brushColors[0]) {
-      return;
-    }
-    cell.brushColor = brushColor;
-
-    cell.style.backgroundColor = ColorGenerator(brushColors[0]);
-  } else if (brushColor == brushColors[1]) {
-    if (cell.brushColor == brushColors[1]) {
-      return;
-    }
-    cell.brushColor = brushColor;
-
-    cell.style.backgroundColor = ColorGenerator(brushColors[1]);
-  } else {
-    if (cell.brushColor == brushColors[2]) {
-      return;
-    }
-    cell.brushColor = brushColor;
-    cell.style.backgroundColor = ColorGenerator(brushColors[2]);
-  }
 }
 
 function colorizeButtons() {
@@ -185,20 +60,98 @@ function colorizeButtons() {
 
   buttons.forEach((button) => {
     button.addEventListener("mouseover", () => {
-      button.style.backgroundColor = ColorGenerator(brushColors[1]);
+      button.style.backgroundColor = ColorGenerator();
     });
     button.addEventListener("mouseout", () => {
-      button.style.backgroundColor = ColorGenerator(brushColors[2]);
+      button.style.backgroundColor = "#fff";
     });
   });
 }
 
-MakeGrid(16);
-AddButtonEvents();
-colorizeButtons();
-changeGridSize();
-document.addEventListener("keydown", ({ key }) => {
-  if (key == "q") {
-    changeBrushType();
-  }
+function changeCurrentColor(newColor) {
+  brushColor = newColor;
+}
+
+function setupBlackBrush() {
+  blackBrushBtn.addEventListener("click", () => {
+    changeCurrentColor("#000");
+  });
+}
+
+function setupRainbowBrush() {
+  rainbowBrushBtn.addEventListener("click", () => {
+    changeCurrentColor("random");
+  });
+}
+
+function setupEraserBrush() {
+  eraserBtn.addEventListener("click", () => {
+    changeCurrentColor("#fff");
+  });
+}
+
+function setupColorPicker() {
+  const colorPicker = document.querySelector("#colorPicker");
+  colorPicker.addEventListener("input", (e) => {
+    const colorValue = e.target.value;
+    changeCurrentColor(colorValue);
+  });
+
+  colorPicker.addEventListener("click", (e) => {
+    const colorValue = e.target.value;
+    changeCurrentColor(colorValue);
+  });
+}
+
+function changeGridSize() {
+  const changeSizeBtn = document.querySelector(".changeSizeBtn");
+  changeSizeBtn.addEventListener("click", () => {
+    gridCellsCount = gridCellsCount === 16 ? 32 : 16;
+    MakeGrid(gridCellsCount);
+  });
+}
+
+function clearBoard() {
+  const clearBoardBtn = document.querySelector(".clearBoardBtn");
+
+  clearBoardBtn.addEventListener("click", () => {
+    gridCells.forEach((gridCell) => {
+        gridCell.setAttribute("randomColor", false);
+      gridCell.style.backgroundColor = "#fff";
+    });
+  });
+}
+
+function fillBoard() {
+  const fillBoardBtn = document.querySelector(".fillBoardBtn");
+  fillBoardBtn.addEventListener("click", () => {
+    gridCells.forEach((gridCell) => {
+      if (brushColor == "random") {
+        gridCell.setAttribute("randomColor", true);
+        gridCell.style.backgroundColor = ColorGenerator();
+      } else {
+        gridCell.setAttribute("randomColor", false);
+        gridCell.style.backgroundColor = brushColor;
+      }
+    });
+  });
+}
+
+function ColorGenerator() {
+  let red = Math.floor(Math.random() * 256),
+    green = Math.floor(Math.random() * 256),
+    blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+MakeGrid(gridCellsCount).then(() => {
+  setupGridCellHoverListener();
+  colorizeButtons();
+  setupBlackBrush();
+  setupRainbowBrush();
+  setupEraserBrush();
+  setupColorPicker();
+  fillBoard();
+  changeGridSize();
+  clearBoard();
 });
